b9159a8acb52d1f2bd7c03bc26f4d051d2d05700
Erich Wenger <erich.wenger@iaik.tugraz.at>
2015-05-20 21:28:25 +0200

    ...

---
+++ run_ecdlps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+export SSHPASS=~/Downloads/sshpass-1.05/sshpass
+
+echo "put *.res" > script
+echo "exit" >> script
+
+while [ 1 ]; do
+
+  killall test_ecdlp
+
+  $SSHPASS -p Bo4hae7eemooy7 sftp ul01@eccbreaker.student.iaik.tugraz.at:/incoming < script
+
+  rm *.res
+
+  COUNTER=0
+  while [  $COUNTER -lt $1 ]; do
+    export filename=$(hostname -f)-$(date +"%y-%m-%d-%T").res
+
+    echo "generating $filename"
+    ./bin/test_ecdlp < ../ecdlp_challenges/ecdlp_ITSEC2015_80.chal > $filename &
+    sleep 1
+    let COUNTER=COUNTER+1 
+  done
+
+  sleep 3600
+
+done
