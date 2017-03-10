eb19c8e3d153f646b5743ad43d77725566a08993
Mario Werner <mario.werner@iaik.tugraz.at>
2017-03-10 14:31:20 +0100

    renamed test file folder

--- include/flecc_in_c/types.h
+++ include/flecc_in_c/types.h
@@ -47,32 +47,42 @@ typedef int64_t slong_t;
 
 /** The number of bytes represented by a word. */
 #define BYTES_PER_WORD 4
+
 /** The number of bits represented by a word. */
 #define BITS_PER_WORD ( BYTES_PER_WORD << 3 )
+
 /** binary logarithm of BITS_PER_WORD*/
 #define LD_BITS_PER_WORD 5
+
 /** binary logarithm of BYTES_PER_WORD*/
 #define LD_BYTES_PER_WORD 2
+
 /** returns the number of words needed to store the defined number of bits */
 #define WORDS_PER_BITS( bits ) ( ( ( bits - 1 ) >> LD_BITS_PER_WORD ) + 1 )
+
 /** returns the number of bytes needed to store the defined number of bits */
 #define BYTES_PER_BITS( bits ) ( ( ( bits - 1 ) >> 3 ) + 1 )
+
 /** the number of bits that has to fit within bigint_t */
 #define MIN_BITS_PER_GFP 521
+
 /** the number of words that fit within bigint_t */
 #define WORDS_PER_GFP ( WORDS_PER_BITS( MIN_BITS_PER_GFP ) )
+
 /** the number of bytes that fit within bigint_t */
 #define BYTES_PER_GFP ( WORDS_PER_GFP * BYTES_PER_WORD )
+
 /** the number of bits that fit within bigint_t */
 #define BITS_PER_GFP ( WORDS_PER_GFP * BITS_PER_WORD )
 
 /** Represent a number in GF(p) - same as bigint_t */
 typedef uint_t gfp_t[WORDS_PER_GFP];
+
 /** Set of parameters needed for general GF(p) operations.
  *  Includes a set of parameters needed for Montgomery GF(p) multiplications.
  *  R is assumed to be (1 << (words * BITS_PER_WORD)).
  */
-typedef struct _gfp_prime_data_t_ {
+typedef struct {
     /** the prime number used for reduction */
     gfp_t prime;
     /** the number of bits needed to represent the prime */
@@ -89,29 +99,37 @@ typedef struct _gfp_prime_data_t_ {
     /** 1*R equals one */
     gfp_t gfp_one;
 } gfp_prime_data_t;
+
 /** Elliptic curve point in affine coordinates. */
-typedef struct _eccp_point_affine_t_ {
+typedef struct {
     gfp_t x;
     gfp_t y;
     uint8_t identity;
 } eccp_point_affine_t;
+
 /** Elliptic curve point using projective (x,y,z) coordinates. */
-typedef struct _eccp_point_projective_t_ {
+typedef struct {
     gfp_t x;
     gfp_t y;
     gfp_t z;
     uint8_t identity;
 } eccp_point_projective_t;
+
 /** specifies the used eccp_parameters_t */
 typedef enum _curve_type_t { UNKNOWN, SECP192R1, SECP224R1, SECP256R1, SECP384R1, SECP521R1, CUSTOM } curve_type_t;
+
 /** function point to a gfp operation (TODO: integrate into eccp_parameters_t) */
 typedef void (*gfp_operation_t)(gfp_t, const gfp_t, const gfp_t, const gfp_prime_data_t*);
+
 /** Parameters needed to do elliptic curve computations. */
 struct _eccp_parameters_t_;
+
 /** typedef of function pointer to an optimized eccp scalar multiplication (used in eccp_parameters_t) */
 typedef void (*eccp_mul_t)(eccp_point_affine_t *,const eccp_point_affine_t*,const gfp_t,const struct _eccp_parameters_t_*);
+
 /** typedef of function pointer to an optimized scalar multiplication with constant point (used in eccp_parameters_t). */
 typedef void (*eccp_mul_const_t)(eccp_point_affine_t *,const gfp_t,const struct _eccp_parameters_t_*);
+
 /** Parameters needed to do elliptic curve computations. */
 typedef struct _eccp_parameters_t_ {
     /** data needed to do computations modulo the prime */
@@ -138,8 +156,9 @@ typedef struct _eccp_parameters_t_ {
     /** optimized scalar multiplication of the base point (uses base_point_tbl) */
     eccp_mul_const_t eccp_mul_base_point;
 } eccp_parameters_t;
+
 /** ECDSA signature, with GF(p) elements modulo ecc_parameters_t.order_n_data */
-typedef struct _ecdsa_signature_t_ {
+typedef struct {
     gfp_t r;
     gfp_t s;
 } ecdsa_signature_t;
